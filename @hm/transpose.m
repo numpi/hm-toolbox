b49638ab93c0013cf5adee51d6ad074a0e7b8961
Leonardo Robol <leo@robol.it>
2017-02-21 12:38:39 +0100

    Added transpose and ctranspose operators.

---
+++ @hm/transpose.m
@@ -0,0 +1,18 @@
+function Ht = transpose(H)
+%TRANSPOSE Transpose of the H-matrix H. 
+
+Ht = H; 
+
+if ~isempty(H.F)
+	Ht = H.F.';
+else
+	Ht.A11 = H.A11.';
+	Ht.A22 = H.A22.';
+	Ht.U12 = conj(H.V21);
+	Ht.V12 = conj(H.U21);
+	Ht.U21 = conj(H.V12);
+	Ht.V21 = conj(H.U12);
+end
+
+end
+
