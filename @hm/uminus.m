7b7c05bc85ef6f560d60e3fc225d44420e606915
Stefano Massei <stefano.massei@sns.it>
2017-02-21 15:06:59 +0100

    added uminus for real

---
+++ @hm/uminus.m
@@ -0,0 +1,11 @@
+function H = uminus(H1)
+%UMINUS Change the sign of a HODLR Matrix 
+if ~isempty(H1.F)
+		H = -H1.F;
+else
+	H = H1;
+	H.A11 = -H1.A11;
+	H.A22 = -H1.A22;
+	H.U21 = -H1.U21;
+	H.U12 = -H1.U12;
+end
