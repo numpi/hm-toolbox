0353b4ed1ebe346755459d8869a46c8926b0bccc
Leonardo Robol <leo@robol.it>
2017-02-21 15:17:21 +0100

    Small performance improvements.

--- @hm/private/compress_matrix.m
+++ @hm/private/compress_matrix.m
@@ -10,13 +10,18 @@ if ~exist('Vold', 'var')
 	U = U(:,1:rk) * sqrt(S(1:rk,1:rk));
 	V = V(:,1:rk) * sqrt(S(1:rk,1:rk));
 else
-	[QU,RU] = qr(Uold, 0);
-	[QV,RV] = qr(Vold, 0);
-	
-	[U,S,V] = svd(RU * RV');
-	rk = sum(diag(S) > threshold);
-	U = QU * U(:,1:rk) * S(1:rk,1:rk);
-	V = QV * V(:,1:rk);
+	if size(Uold, 2) < 16
+		U = Uold;
+		V = Vold;
+	else
+		[QU,RU] = qr(Uold, 0);
+		[QV,RV] = qr(Vold, 0);
+
+		[U,S,V] = svd(RU * RV');
+		rk = sum(diag(S) > threshold);
+		U = QU * U(:,1:rk) * S(1:rk,1:rk);
+		V = QV * V(:,1:rk);
+	end
 end
 
 end
