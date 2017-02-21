56e5f767113b22e9a18f108c1ad98d16585418ef
Leonardo Robol <leo@robol.it>
2017-02-21 12:31:58 +0100

    Added the function size().

---
+++ @hm/size.m
@@ -0,0 +1,18 @@
+function sz = size(H, idx)
+%SIZE Get the size of an H-matrix. 
+
+if ~exist('idx', 'var')
+	sz = H.sz;
+else
+	switch idx
+		case 1
+			sz = H.sz(1);
+		case 2
+			sz = H.sz(2);
+		otherwise
+			error('Unsupported index specified');
+	end
+end
+
+end
+
