80eadf3f603dc239dea85d2b01d39e245ef46d8f
Leonardo Robol <leo@robol.it>
2017-02-24 12:42:08 +0100

    Fixed a bug in mrdivide.

--- @hm/mrdivide.m
+++ @hm/mrdivide.m
@@ -2,6 +2,6 @@ function H = mrdivide(H1, H2)
 %MRDIVIDE solve linear systems with HODLR-matrices
 if isa(H2,'hm') 
 	H = (H2'\H1')';
-elseif isscalar(H1)
-	H = H2 *(1/H1);
+elseif isscalar(H2)
+	H = H1 *(1/H2);
 end
