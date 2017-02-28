d7e8b06ec7d7afb9a21a9597c9ff6949a49d1718
Leonardo Robol <leo@robol.it>
2017-02-28 11:02:20 +0100

    Added a debug option.

--- @hm/lyap.m
+++ @hm/lyap.m
@@ -1,7 +1,13 @@
-function X = lyap(A, C)
+function X = lyap(A, C, debug)
 %LYAP Solve the Lyapounov equation AX + XA' + C = 0
 
-[x, w] = hm_legpts(32);
+N = 32;
+
+[x, w] = hm_legpts(N);
+
+if ~exist('debug', 'var')
+	debug = false;
+end
 
 % Acceleration parameter. 
 L = 100;
@@ -11,6 +17,8 @@ nrm = norm(A);
 % Perform the change of variables and weights
 ww = (2 * L * w) .* ( sin(x) ./ (1 - cos(x)).^2 );
 xx = L * cot(x ./ 2).^2;
+
+nevals = 0;
 
 X = partialSum(xx, ww, @f);
 
@@ -31,7 +39,12 @@ X = partialSum(xx, ww, @f);
 
 	function Y = f(x)	  
 	  eA = expm(-x*A, 'pade', 8, nrm * abs(x));
-	  Y = -eA * C * eA';	  
+	  nevals = nevals + 1;
+	  Y = -eA * C * eA';
+	  
+	  if debug
+		fprintf (' :: %d out of %d evaluations performed\n', nevals, N);
+	  end
 	end
 
 end
