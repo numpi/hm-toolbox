7c6ca66b93dfddf30f694fef130cc2f1a7fab576
Leonardo Robol <leo@robol.it>
2017-05-26 14:49:12 +0200

    Added the possibility to adjust the truncation threshold.

--- @hm/private/rational.m
+++ @hm/private/rational.m
@@ -6,7 +6,7 @@ function [R,P,K]=rational(nu,type)
 %      A(s)     s - P(1)   s - P(2)         s - P(n)
 %
 % type =1 cheb, else  Pade'
-if (type==1),  %Cheb  (only nu=10 at the moment)
+if (type==1)  %Cheb  (only nu=10 at the moment)
     [B,A]=cheby(nu);
 else           %Pade
     [B,A]=coeff(nu);
--- hmoption.m
+++ hmoption.m
@@ -3,17 +3,29 @@ function opt = hmoption(key, value)
 %
 % Valid options are: 
 %   'block-size': Integer representing the minimum block size. 
+%   'threshold': Value used for off-diagonal truncation. 
 
 global hm_block_size
+global hm_threshold
 
 if isempty(hm_block_size)
 	hm_block_size = 128;
+end
+
+if isempty(hm_threshold)
+	hm_threshold = 1e-12;
+end
+
+if ~exist('key', 'var')
+	error('Please specify a key');
 end
 
 if ~exist('value', 'var')
 	switch key
 		case 'block-size'
 			opt = hm_block_size;
+		case 'threshold'
+			opt = hm_threshold;
 		otherwise
 			error('Unsupported option specified');
 	end
@@ -24,6 +36,12 @@ else
 				error('minimum block size must be at least 3');
 			else
 				hm_block_size = value;
+			end
+		case 'threshold'
+			if value < 0
+				error('threshold has to be positive');
+			else
+				hm_threshold = max(eps, value);
 			end
 		otherwise
 			error('Unsupported option specified');
