03fe1262ec782d17b0e3b7fde3d30cbf520f7bba
Leonardo Robol <leo@robol.it>
2017-06-29 19:39:53 +0200

    Added stub of eigs for H-matrices.

---
+++ @hm/eigs.m
@@ -0,0 +1,37 @@
+function [V, D] = eigs(varargin)
+%EIGS Compute extremal eigenvalues of an H-matrix. 
+
+p = inputParser;
+
+p.addRequired('H');
+p.addOptional('k', 5);
+p.addOptional('opts', 'lm');
+
+p.parse(varargin{:});
+args = p.Results;
+
+n = size(args.H, 2);
+
+switch args.opts
+    case { 'lm', 'LM' }
+        if nargout == 2
+            [V, D] = eigs(@(x) args.H * x, n, args.k, args.opts);
+        else
+            V = eigs(@(x) args.H * x, n, args.k, args.opts);
+        end
+        
+    case { 'sm', 'SM' }
+        if nargout == 2
+            [V, D] = eigs(@(x) args.H \ x, n, args.k, args.opts);
+            D = 1 ./ D;
+        else
+            V = eigs(@(x) args.H \ x, n, args.k, args.opts);
+            V = 1 ./ V;
+        end                
+        
+    otherwise
+        error('Unsupported eigenvalue selection');
+end
+
+end
+
