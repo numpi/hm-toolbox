f7ba1305d157858808809a40b0e258c6c0a8535e
Leonardo Robol <leo@robol.it>
2017-09-02 10:43:39 +0200

    Updated hmgallery to reflect the changes in hssgallery.

--- hmgallery.m
+++ hmgallery.m
@@ -1,4 +1,4 @@
-function H = hmgallery(name, varargin)
+function varargout = hmgallery(name, varargin)
 %HMGALLERY Generate examples of H-matrices. 
 %
 % H = HMGALLERY('laplacian', n) generates a n x n discretization of the
@@ -13,8 +13,46 @@ switch name
             n = varargin{1};            
         end
         
-        H = hm('tridiagonal', ...
+        varargout{1} = hm('tridiagonal', ...
             spdiags((n - 1)^2 * ones(n,1) * [ -1 2 -1 ], -1:1, n, n));
+		
+case 'haber'
+		if isempty('varargin')
+			n = 2048;
+		else
+			n = varargin{1};		
+		end
+		
+		m = 6 * n;
+		
+		% We first generate the matrices A and P as sparse matrices. 
+		A = sparse(m, m);
+		P = sparse(m, m);
+
+		a = -1.36;
+		e = 0.34;
+
+		% We build the diagonal blocks
+		for i = 1 : n
+			range = ((i-1)*6+1) : i*6;
+			A(range, range) = spdiags(ones(6,1) * [ e, a, e ], -1:1, 6, 6);
+			P(range, range) = -.2 * ones(6) - .8 * eye(6);
+		end
+
+		% And now super and sub-diagonals
+		for i = 1 : n - 1
+			range = ((i-1)*6+1) : i*6;
+
+			A(range, range + 6) = e * speye(6);
+			A(range + 6, range) = e * speye(6);
+
+			P(range, range + 6) = -.1 * ones(6);
+			P(range + 6, range) = -.1 * ones(6);
+		end
+		
+		varargout{1} = hm('banded', A, 6);
+		varargout{2} = hm('banded', P, 11);
+		
     otherwise
         error('Unsupported example name');
 end
