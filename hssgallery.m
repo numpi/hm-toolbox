898bb35adff7af49d70ca69af0373d5a5fbb430d
Leonardo Robol <leo@robol.it>
2017-09-02 10:41:28 +0200

    Added Haber and Veragen matrices in HSS gallery.

--- hssgallery.m
+++ hssgallery.m
@@ -1,7 +1,7 @@
-function H = hssgallery(name, varargin)
+function varargout = hssgallery(name, varargin)
 %HMGALLERY Generate examples of H-matrices. 
 %
-% H = HMGALLERY('laplacian', n) generates a n x n discretization of the
+% H = HSSGALLERY('laplacian', n) generates a n x n discretization of the
 % Laplacian operator on [-1, 1] using a grid of n+1 points, assuming 
 % Dirichlet boundary conditions. 
 
@@ -13,8 +13,46 @@ switch name
             n = varargin{1};            
         end
         
-        H = hss('banded', ...
+        varargout{1} = hss('banded', ...
             spdiags(ones(n,1) * [ -1 2 -1 ], -1:1, n, n), 1, 1);
+		
+	case 'haber'
+		if isempty('varargin')
+			n = 2048;
+		else
+			n = varargin{1};		
+		end
+		
+		m = 6 * n;
+		
+		% We first generate the matrices A and P as sparse matrices. 
+		A = sparse(m, m);
+		P = sparse(m, m);
+
+		a = -1.36;
+		e = 0.34;
+
+		% We build the diagonal blocks
+		for i = 1 : n
+			range = ((i-1)*6+1) : i*6;
+			A(range, range) = spdiags(ones(6,1) * [ e, a, e ], -1:1, 6, 6);
+			P(range, range) = -.2 * ones(6) - .8 * eye(6);
+		end
+
+		% And now super and sub-diagonals
+		for i = 1 : n - 1
+			range = ((i-1)*6+1) : i*6;
+
+			A(range, range + 6) = e * speye(6);
+			A(range + 6, range) = e * speye(6);
+
+			P(range, range + 6) = -.1 * ones(6);
+			P(range + 6, range) = -.1 * ones(6);
+		end
+		
+		varargout{1} = hss('banded', A, 6, 6);
+		varargout{2} = hss('banded', P, 11, 11);
+		
     otherwise
         error('Unsupported example name');
 end
