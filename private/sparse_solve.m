d317ad3fed78d01b2f722b699b5562009280b5df
Leonardo Robol <leo@robol.it>
2017-10-26 16:55:12 +0200

    Small fixes.

--- private/sparse_solve.m
+++ private/sparse_solve.m
@@ -1,12 +1,8 @@
-function y = sparse_solve(nu, mu, L, U, p, q, x, A)
+function y = sparse_solve(nu, mu, L, U, p, q, x)
 
-% [nu mu]
 if nu > mu
     y = nu\(q*(U\(L\(p*x))));
 else
     y = -mu \ x;
 end
-
-
-% [mu nu]
 
