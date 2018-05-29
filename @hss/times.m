d363b84dc993658b2e52034e5732cd941880694b
Stefano Massei <stefano.massei@sns.it>
2018-05-29 15:58:10 +0200

    hadamard product for hss

---
+++ @hss/times.m
@@ -0,0 +1,11 @@
+function C=times(A,B)
+	if isa(B,'hss')
+		if isa(A,'hss')
+			C = hss_hadamard_mul(A,B);
+		else
+			error('Unsupported Hadamard product between hss and dense matrix')
+		end
+	else
+			error('Unsupported Hadamard product between hss and dense matrix')
+		end
+end
