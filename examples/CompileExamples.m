220fff77f66007294fcd8dcdd38d75b1f38b1091
Leonardo Robol <leo@robol.it>
2018-06-25 18:34:30 +0200

    Added script to compile the examples.

---
+++ examples/CompileExamples.m
@@ -0,0 +1,35 @@
+function html_file = CompileExamples
+%COMPILEGUIDE Compile the toolbox guide from .m files.
+
+% Create a parpool if available, so we don't skew timings later on
+parfor i = 1 : 10
+    % Nothing to do here
+end
+
+if exist('stylesheet.xsl', 'file')
+    publish_cmd = @(file) publish(file, 'stylesheet', 'stylesheet.xsl');
+else
+    publish_cmd = @(file) publish(file);
+end
+
+doc_files = { ...
+    'hm_linear_system.m', ...
+    'hm_lyapunov.m', ...
+    'hss_lyapunov.m'
+    };
+
+main_file = 'examples.m';
+
+for i = 1 : length(doc_files)
+    publish_cmd(doc_files{i});
+end
+
+html_file = publish_cmd(main_file);
+
+if exist('numpi.css', 'file')
+    copyfile('numpi.css', 'html/numpi.css');
+end
+
+
+end
+
