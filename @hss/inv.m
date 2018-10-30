51123c7d2c535e8f4f79bdb55ef87cbf0922d20b
Leonardo Robol <leo@robol.it>
2018-10-30 11:00:05 +0100

    Added @hss/inv.m.

---
+++ @hss/inv.m
@@ -0,0 +1,10 @@
+function B = inv(A)
+%INV Compute the inverse of an HSS matrix. 
+
+if size(A, 1) ~= size(A, 2)
+  error('This matrix is not square');
+end
+
+B = A \ eye(size(A), 'like', A);
+
+end
