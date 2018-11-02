c2ce54b4bb72e33d2dae77afc34f71f128e5fccb
Leonardo Robol <leo@robol.it>
2018-11-02 15:09:17 +0100

    Some changes to @hss/mldivide and @hss/mrdivide for Octave compatibility.

--- @hss/mldivide.m
+++ @hss/mldivide.m
@@ -1,20 +1,24 @@
 function X = mldivide(A, B)
 
-if isscalar(A)
-    X = hss_scalar_mul(1 ./ A, B);
-    return;
-end
-
 if isa(B,'hss')
-    X = hss_mldivide(A, B);
-else
-    if size(A, 1) <= hssoption('block-size')
-        X = full(A) \ B;
-        return;
+    if ~isa(A, 'hss') && isscalar(A)
+        X = hss_scalar_mul(1 / A, B);
+    else
+        X = hss_mldivide(A, B);
     end
-    %[D,U,R,BB,W,V,tr] = hss2xia(A);
-    %X = hssulvsol(tr,D,U,R,BB,W,V,length(tr),B);
-    % X = hss_solve(A, B);
-    X = hss_ulv_solve(A,B);
+else
+    if isa(A, 'hss')
+        if size(A, 1) <= hssoption('block-size')
+            X = full(A) \ B;
+            return;
+        end
+        
+        X = hss_ulv_solve(A,B);        
+    else
+        if isscalar(A)
+            X = hss_scalar_mul(1 / A, B);
+        end
+    end
+
 end
 end
--- @hss/mrdivide.m
+++ @hss/mrdivide.m
@@ -1,10 +1,14 @@
 function X = mrdivide(A, B)
-    if isscalar(B)
-        X = hss_scalar_mul(1 ./ B, A);
-    else
-        X = mldivide(B', A')';        
-    end
-        
 
+if isa(B, 'hss')
+    X = (B' \ A')';        
+else
+    if isscalar(B)
+        X = hss_scalar_mul(1 / B, A);
+        return;
+    end
+    
+    X = (B' \ A')';
+end
 
 end
