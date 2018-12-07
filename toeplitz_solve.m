98c989490f736e516b586f53e8cba0022db8e9ab
Leonardo Robol <leo@robol.it>
2018-12-07 11:53:49 +0100

    Small changes in toeplitz_solve.m

--- toeplitz_solve.m
+++ toeplitz_solve.m
@@ -15,25 +15,23 @@ function x = toeplitz_solve(c, r, b)
 
 n = length(c);
 
-omega = exp(1i * pi / n .* (0 : n - 1));
+d0 = exp(1i * pi / n .* (0 : n - 1));
+d1  = d0.^2;
+dm1 = exp(1i * pi / n) * d1;
 
-G = [ 1 , 2*r(1) ; zeros(n-1, 1), r(end:-1:2).' + c(2:end) ]; % G(1,2) = 2 * c(1);
+G = [ 1 , 2*r(1) ; zeros(n-1, 1), r(end:-1:2).' + c(2:end) ];
 H = [ c(end:-1:2) - r(2:end).' , zeros(n-1,1) ; 0 1 ];
 
 Gh = ifft(G) * sqrt(n);
-Fh = ifft((omega.' * ones(1, 2)) .* H) * sqrt(n);
+Fh = ifft((d0.' * ones(1, 2)) .* H) * sqrt(n);
 
-dp = exp(1i * pi / n * (0 : 2 : 2*n-2));
-dd = exp(1i * pi / n * (1 : 2 : 2*n));
-
-C = hss('handle', @(v) toep_cauchy_matvec(c, r, omega, v), ...
-          @(v) toep_cauchy_matvec_trasp(c, r, omega, v), ...
-          @(i,j) (Gh(i, :) * Fh(j, :)') ./ (dp(i).' - dd(j)), n, n);
+C = hss('handle', @(v) toep_cauchy_matvec(c, r, d0, v), ...
+          @(v) toep_cauchy_matvec_trasp(c, r, d0, v), ...
+          @(i,j) (Gh(i, :) * Fh(j, :)') ./ (d1(i).' - dm1(j)), n, n);
 
 fv = ifft(b);
 fx = C \ fv;
-
-x = omega' .* fft(fx);
+x = d0' .* fft(fx);
 
 end
 
