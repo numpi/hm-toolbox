72248d280b765ee27c309f17c72923696c87c2d2
Leonardo Robol <leo@robol.it>
2019-01-08 10:33:38 +0100

    Added det and a check in trace.

---
+++ @hm/det.m
@@ -0,0 +1,29 @@
+function d = det(H)
+%DET Determinant of a HODLR matrix. 
+%
+% D = DET(H) returns the determinant of H obtained by an LU factorization.
+
+if size(H, 1) ~= size(H, 1)
+    error('det(H): Matrix H is not square');
+end
+
+[L, U] = lu(H);
+
+% The LU of the diagonal blocks is done using a pivoting strategy -- so we
+% cannot assume that det(L) == 1
+d = det_rec(U) * det_rec(L);
+
+end
+
+function d = det_rec(U)
+    if ~isempty(U.F)
+        if size(U, 1) ~= size(U, 2)
+            error('det(H): Diagonal blocks need to be square');
+        end
+        
+        d = det(U.F);
+    else
+        d = det(U.A11) * det(U.A22);
+    end
+end
+
--- @hm/trace.m
+++ @hm/trace.m
@@ -1,5 +1,10 @@
 function t = trace(H)
-%TRACE conputes the trace of a HODLR matrix
+%TRACE computes the trace of a HODLR matrix
+
+if size(H, 1) ~= size(H, 2)
+    error('Trace is supported only for square matrices');
+end
+
 if ~isempty(H.F)
     t = trace(H.F);
 else
