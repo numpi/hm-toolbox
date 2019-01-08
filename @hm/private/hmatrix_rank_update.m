3b269a62b695e7c80e0b2fe075122fcda5ed227f
Leonardo Robol <leo@robol.it>
2019-01-08 11:52:38 +0100

    Added documentation and absolute threshold to hmatrix_rank_update.

--- @hm/private/hmatrix_rank_update.m
+++ @hm/private/hmatrix_rank_update.m
@@ -1,7 +1,12 @@
-function H = hmatrix_rank_update(H, U, V)
+function H = hmatrix_rank_update(H, U, V, nrm)
 %HMATRIX_RANK_UPDATE Perform a low rank update to H.
-
-% H = H + hm('low-rank', U, V);
+%
+% H = HMATRIX_RANK_UPDATE(H, U, V) computes an HM representation of the
+%     matrix H + U * V'; Truncation is performed on each off-diagonal block
+%     with respect to its 2-norm.
+%
+% H = HMATRIX_RANK_UPDATE(H, U, V, NRM) truncates the result of H + U*V'
+%     with respect to the global threshold NRM * HMOPTION('threshold'); 
 
 if ~isempty(H.F)
     H.F = H.F + U * V.';
@@ -9,15 +14,17 @@ else
     m1 = H.A11.sz(1);
     n1 = H.A11.sz(2);
     
-    % H.U12 = [ H.U12, U(1:mp,:) ];
-    % H.V12 = [ H.V12, V(mp+1:end,:) ];
-    [H.U12, H.V12] = compress_factors([ H.U12, U(1:m1,:) ], ...
-        [ H.V12, V(n1+1:end,:) ]);
-    
-    % H.U21 = [ H.U21, U(mp+1:end,:) ];
-    % H.V21 = [ H.V21, V(1:mp,:) ];
-    [H.U21, H.V21] = compress_factors([ H.U21, U(m1+1:end,:) ], ...
-        [ H.V21, V(1:n1,:) ]);
+    if exist('nrm', 'var')
+        [H.U12, H.V12] = compress_factors([ H.U12, U(1:m1,:) ], ...
+            [ H.V12, V(n1+1:end,:) ], nrm);
+        [H.U21, H.V21] = compress_factors([ H.U21, U(m1+1:end,:) ], ...
+            [ H.V21, V(1:n1,:) ], nrm);
+    else
+        [H.U12, H.V12] = compress_factors([ H.U12, U(1:m1,:) ], ...
+            [ H.V12, V(n1+1:end,:) ]);
+        [H.U21, H.V21] = compress_factors([ H.U21, U(m1+1:end,:) ], ...
+            [ H.V21, V(1:n1,:) ]);
+    end
     
     H.A11 = hmatrix_rank_update(H.A11, U(1:m1,:), V(1:n1,:));
     H.A22 = hmatrix_rank_update(H.A22, U(m1+1:end,:), V(n1+1:end,:));
