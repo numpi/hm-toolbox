d16e604ac9d3f3937d6663eff3bb3b1d856a8531
Stefano Massei <stefano.massei@sns.it>
2019-10-30 14:04:17 +0100

    adding basic utilities

---
+++ @hmatrix/conj.m
@@ -0,0 +1,21 @@
+function Ht = conj(H)
+%CONJ Conjugate of HMATRIX matrix H.
+
+Ht = H;
+
+if is_leafnode(H)
+    if H.admissible
+	H.U = conj(U);
+	H.V = conj(V);
+    else
+    	Ht.F = conj(H.F);
+    end
+else
+    Ht.A11 = conj(H.A11);
+    Ht.A12 = conj(H.A12);
+    Ht.A21 = conj(H.A21);
+    Ht.A22 = conj(H.A22);
+end
+
+end
+
