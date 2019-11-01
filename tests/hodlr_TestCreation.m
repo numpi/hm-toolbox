89f33eb89800602a1feaa188aec3ce7d4814f20e
Leonardo Robol <leo@robol.it>
2019-11-01 08:49:24 +0100

    Same relaxing for HODLR Cauchy.

--- tests/hodlr_TestCreation.m
+++ tests/hodlr_TestCreation.m
@@ -30,12 +30,12 @@ for n = [ 100, 1000 ]
 
 	H = hodlr(A);
 
-	CheckTestResult(norm(A - full(H)), '<', norm(A) * hodlroption('threshold'), ...
+	CheckTestResult(norm(A - full(H)), '<', 10 * norm(A) * hodlroption('threshold'), ...
 		'Generation of an hodlr representation for Cauchy A built from dense');
     
     H = hodlr('cauchy', -y, x);
     
-    CheckTestResult(norm(A - full(H)), '<', log2(n) * norm(A) * hodlroption('threshold'), ...
+    CheckTestResult(norm(A - full(H)), '<', 10 * log2(n) * norm(A) * hodlroption('threshold'), ...
 		'Generation of an hodlr representation for Cauchy A built with the Cauchy constructor');
 end
 
