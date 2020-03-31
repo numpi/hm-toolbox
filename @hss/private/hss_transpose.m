7ca22492d7d9951b887f7c4ce038dcc8957355ba
Stefano <massei.stef@gmail.com>
2020-03-31 17:03:36 +0200

    same bug for transpose

--- @hss/private/hss_transpose.m
+++ @hss/private/hss_transpose.m
@@ -14,13 +14,16 @@ else
         B.A11.Rr = conj(A.A11.Wr);
         B.A11.Wl = conj(A.A11.Rl);
         B.A11.Wr = conj(A.A11.Rr);
+    else
+	B.A11.U = conj(A.A11.V);
+        B.A11.V = conj(A.A11.U);
+    end
+    if B.A22.leafnode == 0
         B.A22.Rl = conj(A.A22.Wl);
         B.A22.Rr = conj(A.A22.Wr);
         B.A22.Wl = conj(A.A22.Rl);
         B.A22.Wr = conj(A.A22.Rr);
     else
-        B.A11.U = conj(A.A11.V);
-        B.A11.V = conj(A.A11.U);
         B.A22.U = conj(A.A22.V);
         B.A22.V = conj(A.A22.U);
     end
