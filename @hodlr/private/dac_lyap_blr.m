0ddf19bf424dd763eefcada18495eef8211e1374
Stefano <massei.stef@gmail.com>
2020-04-25 10:13:00 +0200

    improvements to ek_sylv

--- @hodlr/private/dac_lyap_blr.m
+++ @hodlr/private/dac_lyap_blr.m
@@ -55,7 +55,7 @@ tol = hmatrixoption('threshold');
 if is_leafnode(C) && C.admissible
     if is_sparse
         if is_lyapunov
-            M = ek_struct(sA, issymmetric(sA));
+            M = ek_struct(sA);
             [X.U, X.V] = ek_sylv(M, M, C.U, C.V, inf, tol, debug, nrmtype, autosplit);
         else
             [X.U, X.V] = ek_sylv(sA, sB, C.U, C.V, inf, tol, debug, nrmtype, autosplit);
