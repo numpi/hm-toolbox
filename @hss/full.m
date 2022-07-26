deec9fe77125c0184058d7aba09ee1c78d02b66c
Stefano <massei.stef@gmail.com>
2022-07-26 15:46:15 +0200

    Improve the HSS full() function.

--- @hss/full.m
+++ @hss/full.m
@@ -1,17 +1,19 @@
-function M = full(A)
-
-m = size(A, 1);
-n = size(A, 2);
-
-if m < n
-    M = full(A')';
-    return;
-end
-
-if A.topnode == 0
-    A.topnode = 1;
-    M = hss_mul(A, eye(n));
-    A.topnode = 0;
-else
-    M = hss_mul(A, eye(n));
+function [M, U, V] = full(H)
+    if is_leafnode(H)
+        M = H.D;
+        U = H.U;
+        V = H.V;
+    else
+        [M11, U1, V1] = full(H.A11);
+        [M22, U2, V2] = full(H.A22);
+        
+        M = [ M11 , U1 * H.B12 * V2' ; U2 * H.B21 * V1', M22 ];
+        
+        if ~H.topnode
+            U = blkdiag(U1, U2) * [ H.Rl ; H.Rr ];
+            V = blkdiag(V1, V2) * [ H.Wl ; H.Wr ];
+        else
+            U = []; V = [];
+        end
+    end
 end
